91783c61aa78848eb4dc4fbd34aa2829fd07730a
Wong Yu Man <wong.eason@outlook.com>
2023-05-30 01:27:53 +1000

    added home page

---
+++ assets/js/resume.js
@@ -0,0 +1,4 @@
+document.getElementById('resumeButton').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href);
+});
---
+++ assets/js/text-typing.js
@@ -0,0 +1,33 @@
+const names = ["Eason Wong", "Yu Man Wong"];
+let count = 0;
+let index = 0;
+let currentName = '';
+let letter = '';
+let isDeleting = false;
+
+(function type() {
+    if (count === names.length) {
+        count = 0;
+    }
+    currentName = names[count];
+    if (isDeleting) {
+        letter = currentName.slice(0, index--);
+    } else {
+        letter = currentName.slice(0, ++index);
+    }
+
+    document.querySelector('#typeEffect').textContent = letter;
+    if (!isDeleting && letter.length === currentName.length) {
+        // Pause at end
+        setTimeout(type, 1000);
+        isDeleting = true;
+    } else if (isDeleting && letter.length === 0) {
+        isDeleting = false;
+        count++;
+        // Pause before start typing
+        setTimeout(type, 500);
+    } else {
+        let typeSpeed = isDeleting ? 200 : 100;
+        setTimeout(type, typeSpeed);
+    }
+}());
